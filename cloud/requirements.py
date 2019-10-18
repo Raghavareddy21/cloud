657dcffe73b5b71d150e44aa0c8e161b0ce1e797
Challa Venkata Raghava Reddy <challavenkataraghavareddy21@gmail.com>
2019-10-18 20:36:42 +0530

    add app in settings

---
+++ cloud/requirements.py
@@ -0,0 +1,8 @@
+sudo apt-get update
+sudo apt-get install git
+git clone https://github.com/Raghavareddy21/cloud
+cd cloud
+source project/bin/activate
+sudo apt-get install python-setuptools python-dev build-essential
+ssudo apt-get install python3-pip
+pip3 install django
